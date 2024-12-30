9d6c12f6ec408b1cec622205edf5206212a18c61
Graham Kelly <gkgoat6700@gmail.com>
2024-12-30 01:20:44 +0000

    post quantum keys and stuff

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,30 @@ fn main() -> std::io::Result<()> {
             std::fs::write(format!("{out}.sk"), s.as_bytes())?;
             std::fs::write(format!("{out}.vk"), s.verifying_key().as_bytes())?;
         }
+        "kem/xwing" => {
+            if let Ok(s) = std::fs::read(format!("{out}.sk")) {
+                return Ok(());
+            }
+            let (sk,pk) = x_wing::generate_key_pair(&mut rand::thread_rng());
+            std::fs::write(format!("{out}.sk"), sk.as_bytes())?;
+            std::fs::write(format!("{out}.vk"), pk.as_bytes())?;
+        }
+        "signing/falcon/1024" => {
+            if let Ok(s) = std::fs::read(format!("{out}.sk")) {
+                return Ok(());
+            }
+            let (sk,pk) = falcon_rust::falcon1024::keygen(rand::random());
+            std::fs::write(format!("{out}.sk"), sk.to_bytes())?;
+            std::fs::write(format!("{out}.vk"), pk.to_bytes())?;
+        }
+        "signing/falcon/512" => {
+            if let Ok(s) = std::fs::read(format!("{out}.sk")) {
+                return Ok(());
+            }
+            let (sk,pk) = falcon_rust::falcon512::keygen(rand::random());
+            std::fs::write(format!("{out}.sk"), sk.to_bytes())?;
+            std::fs::write(format!("{out}.vk"), pk.to_bytes())?;
+        }
         "git/init" => {
             let mut r = std::fs::read(format!("{out}/.gitignore")).unwrap_or_else(|_| vec![]);
             static X: &[u8] = b"\n*.sk\n!test.sk\n";
@@ -21,7 +45,7 @@ fn main() -> std::io::Result<()> {
             std::fs::write(format!("{out}/.gitignore"), r)?;
         }
         _ => {
-            eprintln!("invalid key type; valid ones are signing/ed25519. you can also initialize a repo with git/init")
+            eprintln!("invalid key type; valid ones are signing/ed25519, kem/xwing, signing/falcon/1024, signing/falcon/512. you can also initialize a repo with git/init")
         }
     };
     Ok(())
