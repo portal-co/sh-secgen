953cf00c1e34d21b3a1547fe73ec3d233dcc5d64
Graham Kelly <gkgoat6700@gmail.com>
2025-03-16 17:22:05 +0000

    pqc transition complete

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use signature::Keypair;
+use slh_dsa::Shake128s;
+
 fn main() -> std::io::Result<()> {
     let mut args = std::env::args();
     args.next();
     let ty = args.next().unwrap();
     let out = args.next().unwrap();
     match ty.as_str() {
-        "signing/ed25519" => {
-            if let Ok(s) = std::fs::read(format!("{out}.sk")) {
-                return Ok(());
-            }
-            let s = ed25519_dalek::SigningKey::generate(&mut rand::thread_rng());
-            std::fs::write(format!("{out}.sk"), s.as_bytes())?;
-            std::fs::write(format!("{out}.vk"), s.verifying_key().as_bytes())?;
-        }
+        // "signing/ed25519" => {
+        //     if let Ok(s) = std::fs::read(format!("{out}.sk")) {
+        //         return Ok(());
+        //     }
+        //     let s = ed25519_dalek::SigningKey::generate(&mut rand::thread_rng());
+        //     std::fs::write(format!("{out}.sk"), s.as_bytes())?;
+        //     std::fs::write(format!("{out}.vk"), s.verifying_key().as_bytes())?;
+        // }
         "kem/xwing" => {
             if let Ok(s) = std::fs::read(format!("{out}.sk")) {
                 return Ok(());
@@ -44,8 +47,16 @@ fn main() -> std::io::Result<()> {
             }
             std::fs::write(format!("{out}/.gitignore"), r)?;
         }
+        "signing/slh/shake128s" => {
+            if let Ok(s) = std::fs::read(format!("{out}.sk")) {
+                return Ok(());
+            }
+            let s = slh_dsa::SigningKey::<Shake128s>::new(&mut rand::thread_rng());
+            std::fs::write(format!("{out}.sk"), s.to_bytes())?;
+            std::fs::write(format!("{out}.vk"), s.verifying_key().to_bytes())?;
+        }
         _ => {
-            eprintln!("invalid key type; valid ones are signing/ed25519, kem/xwing, signing/falcon/1024, signing/falcon/512. you can also initialize a repo with git/init")
+            eprintln!("invalid key type; valid ones are signing/slh/shake128s, kem/xwing, signing/falcon/1024, signing/falcon/512. you can also initialize a repo with git/init")
         }
     };
     Ok(())
